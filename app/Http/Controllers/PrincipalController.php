a580342c0744f90abe13b768dedb8033cf0bc7db
Gustavo Almansa <gustavosalmansa@gmail.com>
2023-06-21 11:50:22 -0300

    Learning about Views

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class PrincipalController extends Controller
 {
     public function principal(){
-        echo "Ola, seja bem vindo ao Laravel";
+        return view('site.principal');
     }
 }
