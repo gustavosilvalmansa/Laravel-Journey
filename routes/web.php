471f2dd035a4c99d06291664ce409d68835fc935
Gustavo Almansa <gustavosalmansa@gmail.com>
2023-06-22 12:25:01 -0300

    naming routes using name->('route.name')

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,16 @@ Route::get('/laravel', function () {
 });
 
 //Call controllers
-Route::get('/','PrincipalController@principal');
-Route::get('/contato','ContatoController@contato');
-Route::get('/sobre-nos','SobreNosController@sobreNos');
-Route::get('/login', function(){return 'login';});
+Route::get('/','PrincipalController@principal')->name('site.index');
+Route::get('/contato','ContatoController@contato')->name('site.contato');
+Route::get('/sobre-nos','SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/login', function(){return 'login';})->name('site.login');
 
+//App
 Route::prefix('/app')->group(function(){
-    Route::get('/clientes', function(){return 'Clientes';});
-    Route::get('/fornecedores', function(){return 'fornecedores';});
-    Route::get('/produtos', function(){return 'produtos';});
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){return 'fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
 });
 
 
