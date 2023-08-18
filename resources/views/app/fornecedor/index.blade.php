8bdeae0b9726baa06ae1243e6c256153c0ae6818
Gustavo Almansa <gustavosalmansa@gmail.com>
2023-08-18 08:04:09 -0300

    Using if sintaxe on blade

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,7 +1,19 @@
 <h3> Fornecedor </h3>
 @php
 
-    
+
 
 
 @endphp
+
+ {{-- @dd($fornecedores) Ler array var --}}
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Menos que 10 fornecedores</h3>
+
+    @elseif(count($fornecedores)> 10)
+        <h3>Mais que 10 fornecedores</h3>
+
+    @else
+        <h3>Sem fornecedores</h3>
+@endif
