dfda8631e39901b75615493dc30225ca4caeda0e
Gustavo Almansa <gustavosalmansa@gmail.com>
2023-09-01 09:44:46 -0300

    migrations and models

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -10,25 +10,8 @@
 
             <div class="informacao-pagina">
                 <div class="contato-principal">
-                    <form action="{{ route('site.contato')}}" method="POST">
-                        @csrf
-                        <input type="text" placeholder="Nome" name="nome" class="borda-preta">
-                        <br>
-                        <input type="text" placeholder="Telefone" name="telefone" class="borda-preta">
-                        <br>
-                        <input type="text" placeholder="E-mail" name="email" class="borda-preta">
-                        <br>
-                        <select class="borda-preta" name ="motivo_contato">
-                            <option value="">Qual o motivo do contato?</option>
-                            <option value="1">Dúvida</option>
-                            <option value="2">Elogio</option>
-                            <option value="3">Reclamação</option>
-                        </select>
-                        <br>
-                        <textarea class="borda-preta" name="mensagem">Preencha aqui a sua mensagem</textarea>
-                        <br>
-                        <button type="submit" class="borda-preta">ENVIAR</button>
-                    </form>
+                    @component('site.layouts._components.form_contato')
+                    @endcomponent
                 </div>
             </div>  
         </div>
